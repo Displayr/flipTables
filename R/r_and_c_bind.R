d5a026509815d0b3e2ca63c1cd00d0e64b776438
TimBock <tim.bock@numbers.net.au>
2017-03-22 14:02:23 +1100

    DS-1108: Rbind and Cbind

--- R/r_and_c_bind.R
+++ R/r_and_c_bind.R
@@ -1,28 +1,67 @@
-#' RBind
+#' rbindAndCbindWithLabels
 #'
-#' Takes a sequence of vectors or tables and merges them, similar to rbind, except that it matches
+#' Takes a sequence of vectors or tables and merges them, similar to rbind and cbind, except that it matches
 #' based on names.
 #' @param ... The tables or vectors to be merged.
-#' @export
-Rbind <- function(...)
+#' @param rows If TRUE, binds by rows. Otherwise by columns.
+#' @param keep.all If TRUE, even non-matching rows are retained.
+rbindAndCbindWithLabels <- function(..., rows, keep.all)
 {
-    rbind.tables <- suppressWarnings(rbind(...))
+    bind <- if (rows) rbind else cbind
+    bind.tables <- suppressWarnings(bind(...))
     tables <- list(...)
     if(is.list(tables[[1]]))
-        return(Rbind(tables[[1]]))
+        return(rbindAndCbinWithLabels(tables[[1]]))
     if (is.null(tables[[1]]))
     {
         tables[[1]] <- NULL
-        return(rbind.tables)
+        return(bind.tables)
     }
     if (!(any(c("names", "dimnames") %in% names(attributes(tables[[1]])))))
     {
         warning("As the first table contains no names, names have been ignored in the matching.")
-        return(rbind(...))
+        return(bind(...))
     }
-    merged <- MergeTables(tables, direction = "Up-and-down", nonmatching = "Keep all")
-    if(is.vector(merged) || nrow(merged) == 1)
-        return(merged)
-    rownames(merged) <- rownames(rbind.tables)
+    merged <- MergeTables(tables, direction = if (rows) "Up-and-down" else "Side-by-side", nonmatching = if (keep.all) "Keep all" else "Matching only")
+    if (rows)
+    {
+        if(is.vector(merged) || nrow(merged) == 1)
+            return(merged)
+        rownames(merged) <- rownames(bind.tables)
+
+    }
+    else
+    {
+        if(is.vector(merged) || ncol(merged) == 1)
+            return(merged)
+        colnames(merged) <- colnames(bind.tables)
+    }
     merged
+}
+
+
+
+#' RBind
+#'
+#' Takes a sequence of vectors or tables and merges them, similar to \code{\link{rbind}}, except that it matches
+#' based on names.
+#' @param ... The tables or vectors to be merged.
+#' @param keep.all If TRUE, even non-matching rows are retained.
+#' @export
+Rbind <- function(..., keep.all = TRUE)
+{
+    rbindAndCbindWithLabels(..., rows = TRUE, keep.all = keep.all)
+}
+
+
+#' CBind
+#'
+#' Takes a sequence of vectors or tables and merges them, similar to \code{\link{rbind}}, except that it matches
+#' based on names.
+#' @param ... The tables or vectors to be merged.
+#' @param keep.all If TRUE, even non-matching rows are retained.
+#' @export
+Cbind <- function(..., keep.all = TRUE)
+{
+    rbindAndCbindWithLabels(..., rows = FALSE, keep.all = keep.all)
 }
--- tests/testthat/test-r_and_c_bind.R
+++ tests/testthat/test-r_and_c_bind.R
@@ -1,4 +1,4 @@
-context("Rbind")
+context("Rbind and Cbind")
 
 test_that("Rbind",
           {
@@ -8,9 +8,36 @@ test_that("Rbind",
               b <- c(a = 1, b = 2)
               expect_equal(Rbind(a, b), rbind(a, b))
               b <- c(a = 1, c = 2)
-              expect_equivalent(Rbind(a, b), matrix(c(1, 1, NA, 2, 3, NA)))
+              expect_equivalent(Rbind(a, b), matrix(c(1, 1, NA, 2, 3, NA), nrow = 2))
               a <- NULL
               b <- c(a = 1, c = 2)
               expect_equivalent(Rbind(a, b), rbind(a, b))
+              a = c("A" = 1, B = 2, D = 3)
+              b = matrix(1:9, nrow = 3, dimnames = list(ROW = c("A", "D", "C"), COLUMNS = 1:3))
+              a.and.b = structure(c(1, 1, 4, 7, 2, NA, NA, NA, 3, 2, 5, 8, NA, 3, 6,  9), .Dim = c(4L, 4L), .Dimnames = list(c("a", "1", "2", "3"),  c("A", "B", "D", "C")))
+              expect_equivalent(Rbind(a, t(b)), a.and.b)
+              a.and.b = a.and.b[,c("A", "D")]
+              expect_equivalent(Rbind(a, t(b), keep.all = FALSE), a.and.b)
+          })
+
+
+test_that("Rbind",
+          {
+              expect_equal(suppressWarnings(Cbind(1:5, 1:5)), cbind(1:5, 1:5))
+              a <- c(a = 1, b = 3)
+              expect_equivalent(Cbind(a), a)
+              b <- c(a = 1, b = 2)
+              expect_equal(Cbind(a, b), cbind(a, b))
+              b <- c(a = 1, c = 2)
+              expect_equivalent(Cbind(a, b), matrix(c(1, 1, NA, 2, 3, NA), nrow = 3, byrow = TRUE))
+              a <- NULL
+              b <- c(a = 1, c = 2)
+              expect_equivalent(Cbind(a, b), cbind(a, b))
+              a = c("A" = 1, B = 2, D = 3)
+              b = matrix(1:9, nrow = 3, dimnames = list(ROW = c("A", "D", "C"), COLUMNS = 1:3))
+              a.and.b = structure(c(1, 1, 4, 7, 2, NA, NA, NA, 3, 2, 5, 8, NA, 3, 6,  9), .Dim = c(4L, 4L), .Dimnames = list(c("a", "1", "2", "3"),  c("A", "B", "D", "C")))
+              expect_equivalent(Cbind(a, b), t(a.and.b))
+              a.and.b = a.and.b[,c("A", "D")]
+              expect_equivalent(Cbind(a, b, keep.all = FALSE), t(a.and.b))
           })
 
