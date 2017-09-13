2075d60da4a15bb791c2be419e5fc56be74b0e52
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-09-13 18:14:01 +1000

    BasicTable working except for additional args

--- R/BasicTable.R
+++ R/BasicTable.R
@@ -1,59 +1,43 @@
 #' Create a BasicTable Object
 #'
 #' Creates tables used by Displayr charting functions
-#' @param x \strong{named} matrix or vector
-#' @param by vector over which y will be aggregated. Must have the same
-#' number of elements as y; see \code{\link[stats]{aggregate}}
+#' @param x a \strong{named} numeric matrix or vector
+#' @param by vector over which y will be aggregated. Must have the
+#'     same number of elements as y; see
+#'     \code{\link[stats]{aggregate}}
 #' @param date optional vector of dates, which if supplied, will be
 #'     used for the row names of the returned table; must be unique
 #'     and have the same length or number of rows of \code{x}; will
 #'     overwrite any existing row names present in \code{x}
+#' @param as.binary logical; if \code{TRUE}, unordered factors in
+#'     \code{x} are represented as dummy variables; otherwise, they
+#'     are represented as sequential integers
 #' @param row.names.to.remove character vector of row labels
 #'     specifying rows to remove from the returned table
 #' @param col.names.to.remove character vector of column labels
 #'     specifying columns to remove from the returned table
-#' @param logical; if \code{TRUE} the table will be transposed before
-#'     being returned00
-#' @importFrom flipTransformations ParseEnteredData
-#' @return An object of class \code{BasicTable} - a named matrix or vector
+#' @param transpose logical; if \code{TRUE} the table will be
+#'     transposed before being returned
+#' @details If \code{x} is not a numeric vector or matrix, an attempt
+#'     will be made to coerce it to one using \code{\link{AsBasicTable}}.
+#' @importFrom flipTransformations RemoveRowsAndOrColumns
+#' @seealso \code{\link{AsBasicTable}}
+#' @return An object of class \code{BasicTable} - a \strong{named}
+#'     matrix or vector
 BasicTable <- function(x, by = NULL, date = NULL,
-                       row.names.to.remove = NULL,
+                       as.binary = FALSE, row.names.to.remove = NULL,
                        col.names.to.remove = NULL,
                        transpose = FALSE)
 {
-    if (is.character(x))  # user entered raw data
-        x <- flipTransformations::ParseEnteredData(x, want.data.frame = FALSE, want.col.names = TRUE,
-                                                   want.row.names = TRUE)
-    if (!is.null(date))
-        x <- tidyDataForStacking(x, date)
+    ## if not given a numeric vector or matrix, try to coerce to one
+    if (!is.numeric(x) || is.null(dims <- dim(x)) || length(dims) > 2 || IsQTable(x))
+        x <- AsBasicTable(x, as.binary = as.binary)
 
-    dim.x <- dim(x)
-    dim.names <- dimnames(x)
-    if (length(dim.x) != 2)
-    {
-        if (length(dim.x) == 3 & !is.null(dim.names))
-        {
-            x <- x[, , 1]
-            warning(gettextf("The analysis has been performed on the first statistic in the table (%s).",
-                           dim.names[[3]][1]))
-            if (is.character(x[1,1]))
-                x <- matrix(as.numeric(x), nrow(x), dimnames = dimnames(x))
-        }
-        else
-        {
-            stop("This analysis requires a two-dimensional table (i.e., a table with one ",
-                 "set of row headings, one set of columns headings, and one statistic in each cell.")
-        }
-    }
-    x <- SetDimNames(x)
-    if (is.null(dim.names))
-    {
-        dimnames(x) <- list(Rows = 1:nrow(x), Columns = 1:ncol(x))
-    }
-    else
-    {
-        x <- RemoveRowsAndOrColumns(x, row.names.to.remove, col.names.to.remove)
-    }
+    ## Handle by arg
+
+    x <- SetDimNames(x, date)
+    x <- RemoveRowsAndOrColumns(x, row.names.to.remove, col.names.to.remove)
+
     class(x) <- c("BasicTable", if (is.null(dim(x))) "numeric" else "matrix")
     x
 }
