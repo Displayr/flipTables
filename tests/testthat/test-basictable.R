c32858338495c991e056761434de706b3d58aa9f
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-09-13 18:16:12 +1000

    started test script for BasicTable

---
+++ tests/testthat/test-basictable.R
@@ -0,0 +1,23 @@
+context("Basic Table")
+
+test_that("BasicTable: works with date arg",
+{
+
+})
+
+test_that("BasicTable: works with by arg",
+{
+
+})
+
+test_that("BasicTable: removes cols or rows properly",
+{
+
+})
+
+
+
+test_that("BasicTable: works when needs to call AsBasicTable first",
+{
+  ## check QTable
+})
