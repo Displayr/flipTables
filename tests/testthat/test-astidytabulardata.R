119cf8b3008528eb945859c6218131a2e1f8cb4e
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-10-24 16:23:11 +1100

    Add additional unit tests for AsTidyTabularData
    * Addresses TB comment on DS-1471
    
    Addresses: DS-1471

--- tests/testthat/test-astidytabulardata.R
+++ tests/testthat/test-astidytabulardata.R
@@ -35,17 +35,45 @@ test_that("AsTidyTabularData: 1D array becomes named vector",
 
 test_that("AsTidyTabularData: works with QDates",
 {
-
+    dat <- structure(list(foo = structure(c(1, 2), name = "UniqueID", label = "Unique Identifier",
+                                          question = "Unique Identifier",
+                                          questiontype = "Number"),
+                    bar = structure(c(3L, 5L),
+                    .Label = c("19-Dec-11-01-Jan-12",
+                                      "02-Jan-12-15-Jan-12", "16-Jan-12-29-Jan-12", "30-Jan-12-12-Feb-12",
+                                      "13-Feb-12-26-Feb-12", "27-Feb-12-11-Mar-12", "12-Mar-12-25-Mar-12",
+                                      "26-Mar-12-08-Apr-12", "09-Apr-12-22-Apr-12", "23-Apr-12-06-May-12",
+                                      "07-May-12-20-May-12", "21-May-12-03-Jun-12", "04-Jun-12-17-Jun-12",
+                                      "18-Jun-12-01-Jul-12", "02-Jul-12-15-Jul-12", "16-Jul-12-29-Jul-12",
+                                      "30-Jul-12-12-Aug-12", "13-Aug-12-26-Aug-12", "27-Aug-12-09-Sep-12",
+                                      "10-Sep-12-23-Sep-12", "24-Sep-12-07-Oct-12", "08-Oct-12-21-Oct-12",
+                                      "22-Oct-12-04-Nov-12", "05-Nov-12-18-Nov-12", "19-Nov-12-02-Dec-12",
+                                      "03-Dec-12-16-Dec-12", "17-Dec-12-30-Dec-12"), class = c("ordered",
+                                                                                               "factor"), name = "date",
+                    label = "Interview Date", question = "Interview Date", questiontype = "Date")),
+               .Names = c("foo", "bar"), row.names = c(NA, 2L), class = "data.frame")
+    expect_warning(out <- AsTidyTabularData(dat), "converted to being numeric")
+    expect_is(out, "matrix")
+    expect_equal(dim(out), dim(dat))
 })
 
 test_that("AsTidyTabularData: as labels to vector",
 {
+    x <- rbind(letters[1:3], 1:3)
+    out <- AsTidyTabularData(x)
+    expect_named(out, x[1, ])
+    expect_equal(out, as.numeric(x[2, ]), check.attributes = FALSE)
 
+    x <- t(x)
+    out <- AsTidyTabularData(x)
+    expect_named(out, x[, 1])
+    expect_equal(out, as.numeric(x[, 2]), check.attributes = FALSE)
 })
 
 test_that("AsTidyTabularData: errors for unknown inputs",
 {
-
+    expect_error(AsTidyTabularData(as.Date("2017-01-01")),
+                 "^Cannot coerce object of type")
 })
 
 test_that("AsTidyTabularData: works for matrices",
