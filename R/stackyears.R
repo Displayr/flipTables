f95337c2c80f9ebc79a41e7d5f40c4c34124c2b9
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-11-08 15:56:36 +1100

    Use new arg. in AsDate; DS-1607

--- R/stackyears.R
+++ R/stackyears.R
@@ -2,20 +2,27 @@
 #'
 #' Stacks years to make seasonal patterns and growth easier to see.
 #'
-#' @param x A \code{\link{vector}}, \code{\link{list}}, \code{\link{table}}. or \code{\link{data.frame}}.
-#' @param date An optional vector containing dates. Where not provided, the dates are assumed to be in the row or column names, first row or column, or first element of \code{x}.
+#' @param x A \code{\link{vector}}, \code{\link{list}}, \code{\link{table}}.
+#' or \code{\link{data.frame}}.
+#' @param date An optional vector containing dates. Where not provided,
+#' the dates are assumed to be in the row or column names, first row or column,
+#' or first element of \code{x}.
 #' @param n.years The number of years to stack. Older data is ignored.
-#' @param calendar If true, the years are calendar. Otherwise, the end at the final time point. When this is done, some years can have different numbers of values (e.g., weeks).
+#' @param calendar If true, the years are calendar. Otherwise, the end at the final
+#' time point. When this is done, some years can have different numbers of
+#' values (e.g., weeks).
 #' @param period.number If TRUE, period numbers instead of dates are returned in the column names.
 #' @param transpose If TRUE, the result is transposed.
-#' @return A \code{\link{matrix}}, with the column names containing the dates or time periods, and the rows the years.
+#' @return A \code{\link{matrix}}, with the column names containing the dates or time periods,
+#' and the rows the years.
 #' @importFrom lubridate year years interval duration
 #' @importFrom flipTime AsDate
 #' @export
-StackYears <- function(x, date = NULL, n.years = NULL, calendar = TRUE, period.number = FALSE, transpose = FALSE)
+StackYears <- function(x, date = NULL, n.years = NULL, calendar = TRUE,
+                       period.number = FALSE, transpose = FALSE)
 {
     x <- TidyTabularData(x, date)
-    date <- AsDate(names(x))
+    date <- AsDate(names(x), on.parse.failure = "silent")
     latest.year <- year(max(date))
     year.index <- if (calendar)
         latest.year - year(date) + 1
