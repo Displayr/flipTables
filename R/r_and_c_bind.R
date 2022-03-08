935326b211637924b71737c6bea8f53d6920e48b
Mathew W. McLean <mathew.w.mclean@gmail.com>
2022-03-08 19:03:22 +1100

    DS-3041: Fix Rbind with vector inputs
    * Add new internal function cbindWithLabels, update Rbind
    and Cbind to use this new function

--- R/r_and_c_bind.R
+++ R/r_and_c_bind.R
@@ -44,6 +44,42 @@ rbindAndCbindWithLabels <- function(..., rows, keep.all)
     merged
 }
 
+#' Takes a sequence of vectors or tables and merges them, similar to
+#' rbind and cbind, except that it matches based on names. If there
+#' are no names, matching is based upon indices.
+#' @param ... The tables or vectors to be merged.
+#' @param keep.all If \code{"TRUE"}, even non-matching rows/columns
+#'     are retained. Ignored and treated as \code{"TRUE"} if matching
+#'     is based upon indices.
+#' @noRd
+cbindWithLabels <- function(..., keep.all)
+{
+    # rbind does not work on matrices with different ncols. However we use rbind to extract rownames.
+    # So temporarily set all ncols to 1 for matrices. Vica versa for cbind.
+    tables <- list(...)
+    tables <- lapply(tables, oneDimensionalArrayToVector)
+    vectors <- unlist(lapply(tables, is.vector))
+    tables[!vectors] <- lapply(tables[!vectors], '[', 1, , drop = FALSE)
+
+    # reset names of vectors which have been lost and are used as rownames.
+    object.names <- as.character(substitute(list(...)))[-1L]
+    named.objects <- sapply(object.names, exists)
+    names(tables)[named.objects] <- object.names[named.objects]
+    bind.tables <- suppressWarnings(do.call(cbind, tables))
+
+    tables <- list(...)   # reset to full size matrices
+    tables <- lapply(tables, oneDimensionalArrayToVector)
+    tables <- tables[!sapply(tables, is.null)]
+
+    if(is.list(tables[[1]]) && !is.data.frame(tables[[1]]))
+        return(cbindWithLabels(tables[[1]], keep.all = keep.all))
+
+    merged <- MergeTables(tables, direction = "Side-by-side",
+                          nonmatching = if (keep.all) "Keep all" else "Matching only")
+
+    colnames(merged) <- colnames(bind.tables)
+    return(merged)
+}
 
 
 #' Rbind
@@ -56,9 +92,12 @@ rbindAndCbindWithLabels <- function(..., rows, keep.all)
 #' @export
 Rbind <- function(..., keep.all = TRUE)
 {
-    rbindAndCbindWithLabels(..., rows = TRUE, keep.all = keep.all)
+    tbls <- lapply(list(...), function(x) if (length(dim(x)) == 2L) t(x) else x)
+    return(t(do.call(cbindWithLabels, c(tbls, keep.all = keep.all))))
 }
 
+RbindOld <- function(..., rows = TRUE, keep.all = TRUE)
+    return(rbindAndCbindWithLabels(..., rows = TRUE, keep.all = keep.all))
 
 #' Cbind
 #'
@@ -70,7 +109,7 @@ Rbind <- function(..., keep.all = TRUE)
 #' @export
 Cbind <- function(..., keep.all = TRUE)
 {
-    rbindAndCbindWithLabels(..., rows = FALSE, keep.all = keep.all)
+        return(cbindWithLabels(..., keep.all = keep.all))
 }
 
 # Converts array with 1 dimension to an equivalent vector, preserving names.
