cd5ecb0e492ca919ce90c9def62f8cc1a8a6d280
Carmen Chan <chschan@gmail.com>
2025-05-20 17:39:54 +1000

    RS-18683: Keep QTable class (#41)

--- R/astidytabulardata.R
+++ R/astidytabulardata.R
@@ -91,7 +91,7 @@ AsTidyTabularData <- function(x, ...)
         x <- AsNumeric(x)
     else if (length(dim(x)) == 1L)
         class(x) <- "numeric"
-    else if (!is.data.frame(x))
+    else if (!is.data.frame(x) && !IsQTable(x))
         class(x) <- "matrix"
     x
 }
